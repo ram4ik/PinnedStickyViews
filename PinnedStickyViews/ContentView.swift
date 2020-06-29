2c8e95324f34bd1c7a8b658396eecc4a481a1c6b
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-06-29 18:57:00 +0300

    Initial Commit

---
+++ PinnedStickyViews/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  PinnedStickyViews
+//
+//  Created by Ramill Ibragimov on 29.06.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PinnedStickyViews/PinnedStickyViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinnedStickyViewsApp.swift
+//  PinnedStickyViews
+//
+//  Created by Ramill Ibragimov on 29.06.2020.
+//
+
+import SwiftUI
+
+@main
+struct PinnedStickyViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
